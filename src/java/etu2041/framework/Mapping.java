62ebb70d2ce37f6cb1e289e842fad893ba926983
ManoaNyavo04 <120180988+ManoaNyavo04@users.noreply.github.com>
2023-03-14 07:43:03 +0100

    Sprint2

---
+++ src/java/etu2041/framework/Mapping.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etu2041.framework;
+
+/**
+ *
+ * @author manou cherie
+ */
+public class Mapping {
+    String className;
+    String methode;
+    
+    public Mapping() {
+    }
+    
+    public Mapping(String className, String methode) {
+        this.className = className;
+        this.methode = methode;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethode() {
+        return methode;
+    }
+
+    public void setMethode(String methode) {
+        this.methode = methode;
+    }
+    
+}
